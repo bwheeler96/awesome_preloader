4e29d3ac7c8ce3850f496d63b99e7f4c978b359f
Brian Wheeler <bwheeler96@gmail.com>
2016-01-21 11:08:59 -0800

    alter usage pattern to assign attributes based on returned hash

--- lib/awesome_preloader.rb
+++ lib/awesome_preloader.rb
@@ -24,7 +24,10 @@ module AwesomePreloader
     def preloads(*method_names)
       klass = self.class.to_s.split('::')[0...-1].join('::').constantize
       method_names.each do |name|
-        klass.preloaders[name].call(self)
+        preload_hash = klass.preloaders[name].call(self)
+        self.each do |obj|
+          obj.send("#{name}=", preload_hash[obj.id])
+        end
       end
       return self
     end
--- test/dummy/app/models/preload_writer_test.rb
+++ test/dummy/app/models/preload_writer_test.rb
@@ -5,8 +5,5 @@ class PreloadWriterTest < ActiveRecord::Base
 
 	preloads :sum_of_items_values do |collection|
 		items_values = PreloadableItemExample.where(preload_writer_test_id: collection.map(&:id)).group(:preload_writer_test_id).pluck(:preload_writer_test_id, 'SUM(value) as value').to_h
-		collection.each do |item|
-			item.sum_of_items_values = items_values[item.id]
-		end
 	end
 end
